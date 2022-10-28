1b3447edb65819e81722d0108c1fc71d4581d292
SAMEER HASHMI <116084722+SAMEER6363@users.noreply.github.com>
2022-10-28 23:14:51 +0500

    Update RAIZ.py

--- RAIZ.py
+++ RAIZ.py
@@ -1,15 +1,2 @@
-import os,time,platform
-os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-green = ('\033[1;32m')
-white = ('\033[1;37m')
-red = ('\033[1;31m')
-
-print('<------------------------------------>')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    print(f'{red}[•] Join Over Facebook Group {white}')
-    os.system('xdg-open https://www.facebook.com/profile.php?id=100086262677107')
-    import v14
+import v14
 v14.sdj()
